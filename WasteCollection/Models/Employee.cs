5960443628c859ad400233b6e101fbfbe08d31ec
Nicholas Radtke <Raddnc@gmail.com>
2020-11-06 19:38:29 -0600

    added employee model

--- WasteCollection/Models/Employee.cs
+++ WasteCollection/Models/Employee.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
 
 namespace WasteCollection.Models
 {
     public class Employee
     {
+        [Key]
+        public int Id { get; set; }
     }
 }
