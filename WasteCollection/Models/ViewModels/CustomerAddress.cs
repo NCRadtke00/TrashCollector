e0b59f95a221fa83eab7547e0388d28afd19e76c
Nicholas Radtke <Raddnc@gmail.com>
2020-11-06 19:53:23 -0600

    added logic to customeraddress, finished

--- WasteCollection/Models/ViewModels/CustomerAddress.cs
+++ WasteCollection/Models/ViewModels/CustomerAddress.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace WasteCollection.Models.ViewModels
 {
     public class CustomerAddress
     {
+        [Display(Name = "Street Address")]
+        public string StreetAddress { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+
+        [Display(Name = "Zip Code")]
+        public int ZipCode { get; set; }
+
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
     }
 }
