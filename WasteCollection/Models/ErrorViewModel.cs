702dc5a0cc7ffa2338a666ed18d1386d853c6cfa
Nicholas Radtke <Raddnc@gmail.com>
2020-11-06 18:39:25 -0600

    created new project

---
+++ WasteCollection/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WasteCollection.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
