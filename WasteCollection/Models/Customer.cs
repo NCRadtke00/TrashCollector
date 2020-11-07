17bd06100e76126f9fbecc6c4a5b6db5d8d51c04
Nicholas Radtke <Raddnc@gmail.com>
2020-11-06 19:02:13 -0600

    created customer class, added address city,state,zipcode

--- WasteCollection/Models/Customer.cs
+++ WasteCollection/Models/Customer.cs
@@ -16,5 +16,9 @@ namespace WasteCollection.Models
         public string LastName { get; set; }
         [Display(Name = "Street Address")]
         public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        [Display(Name = "Zip Code")]
+        public int ZipCode { get; set; }
     }
 }
