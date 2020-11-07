8b527c4c8b1b23256c572a0d98c512b70bf2d3f7
Nicholas Radtke <Raddnc@gmail.com>
2020-11-06 19:37:03 -0600

    finished adding logic to customer model.

--- WasteCollection/Models/Customer.cs
+++ WasteCollection/Models/Customer.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -22,14 +23,36 @@ namespace WasteCollection.Models
         public string State { get; set; }
         [Display(Name = "Zip Code")]
         public int ZipCode { get; set; }
-        [Display(Name = "Current Balance")]
-        [DisplayFormat(DataFormatString = "{0:C}")]
-        public double CurrentBalance { get; set; }
 
         [ForeignKey("PickUpDay")]
         [Display(Name = "Pickup Day")]
         public int PickUpDayId { get; set; }
         public PickUpDay PickUpDay { get; set; }
+        [Display(Name = "Additional PickUp date")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime? AdditionalPickUDay { get; set; }
+        [NotMapped]
+        public string AdditionalPickUDayString { get; set; }
+        [NotMapped]
+        public SelectList Days { get; set; }
+        public bool IsAccountSuspended { get; set; }
+
+        [Display(Name = "Account Suspension Start Date")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime? AccountSuspendStartDate { get; set; }
+
+        [Display(Name = "Account Suspension End Date")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime? AccountSuspendEndDate { get; set; }
+
+        [NotMapped]
+        public DateTime? MostRecentChargedDay { get; set; }
+        [Display(Name = "Current Balance")]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        public double CurrentAccountBalance { get; set; }
+        [Display(Name = "Check the box to confirm trash has been pick up!")]
+        public bool ConfirmTrashPickUp { get; set; }
+
 
         [ForeignKey("IdentityUser")]
         public string IdentityUserId { get; set; }
