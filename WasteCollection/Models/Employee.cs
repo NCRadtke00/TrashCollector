73046cc9a5186905e6ec556216299520ae709d9f
Nicholas Radtke <Raddnc@gmail.com>
2020-11-06 19:41:02 -0600

    added pk id, name and fk identityuserid to employee model

--- WasteCollection/Models/Employee.cs
+++ WasteCollection/Models/Employee.cs
@@ -13,5 +13,13 @@ namespace WasteCollection.Models
     {
         [Key]
         public int Id { get; set; }
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+
     }
 }
