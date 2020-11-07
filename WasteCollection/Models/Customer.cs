563bf8f0c20498b799d0d6c1b55836d1b63de388
Nicholas Radtke <Raddnc@gmail.com>
2020-11-06 19:01:09 -0600

    created customer class, added lastname

--- WasteCollection/Models/Customer.cs
+++ WasteCollection/Models/Customer.cs
@@ -12,5 +12,7 @@ namespace WasteCollection.Models
         public int Id { get; set; }
         [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
     }
 }
