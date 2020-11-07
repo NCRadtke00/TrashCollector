daeaeba49e445657f76b27921e3199e25caa674e
Nicholas Radtke <Raddnc@gmail.com>
2020-11-06 19:06:43 -0600

    adding logic to globalrouting class

--- WasteCollection/ActionFilters/GlobalRouting.cs
+++ WasteCollection/ActionFilters/GlobalRouting.cs
@@ -1,11 +1,28 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace WasteCollection.ActionFilters
 {
-    public class GlobalRouting
+    public class GlobalRouting : IActionFilter
     {
+        private readonly ClaimsPrincipal _claimsPrincipal;
+        public GlobalRouting(ClaimsPrincipal claimsPrincipal)
+        {
+            _claimsPrincipal = claimsPrincipal;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
